c31523baac183ab182fa1965173c7fac622c2840
EngAndresZambrano <zandres88@gmail.com>
2025-04-17 14:39:58 +0000

    Deploying to gh-pages from @ EngAndresZambrano/engandreszambrano.github.io@a2b525a6a54abd65bbd78b532a46131eebc669a6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -69,6 +69,11 @@ ninja.data = [{
           description: "This project is based on the research and presentation of the most relevant information security frameworks and policies applied within a business environment. It highlights essential standards such as NIST, ISO/IEC, and COBIT, offering a structured overview of their roles in managing cyber risks and strengthening organizational security posture.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
+            },},{id: "projects-project-8",
+          title: 'project 8',
+          description: "an other project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/8_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
